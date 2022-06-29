69c360ac355910911380eadd2cada8c407c2448b
Chloe Konde <konde.chloe@gmail.com>
2022-06-29 18:30:47 +0500

    refactor main

--- src/main/kotlin/com/chloe/kotlinserv/MainVerticle.kt
+++ src/main/kotlin/com/chloe/kotlinserv/MainVerticle.kt
@@ -1,32 +1,29 @@
 package com.chloe.kotlinserv
 
 import com.google.gson.Gson
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.Promise
-import io.vertx.core.http.HttpServer
+import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.ext.web.Router
 
-class MainVerticle : AbstractVerticle() {
+fun main(args: Array<String>) {
+    val vertx = Vertx.vertx()
+    val httpServer = vertx.createHttpServer()
+    val router = Router.router(vertx)
+    val gson = Gson()
 
-    override fun start(startPromise: Promise<Void>) {
-        val server: HttpServer = vertx.createHttpServer()
-        val router: Router = Router.router(vertx)
-        val gson = Gson()
-
-        router.get("/countrystats/").handler { ctx ->
-            val response: HttpServerResponse = ctx.response()
-            response.putHeader("content-type", "application/json")
-            val data = CountryStats("23-12-2000", "RUS", 3)
-            response.end(gson.toJson(data))
-        }
-        router.post("/geodata").handler { ctx ->
-            val response: HttpServerResponse = ctx.response()
-            response.putHeader("content-type", "application/json")
-            val data = GeoData("GB", 3243242, "myTestUser")
-            response.end(gson.toJson(data))
-        }
-
-        server.requestHandler(router).listen(8080)
+    router.get("/countrystats/").handler { ctx ->
+        val response: HttpServerResponse = ctx.response()
+        response.putHeader("content-type", "application/json")
+        val data = CountryStats("23-12-2000", "RUS", 3)
+        response.end(gson.toJson(data))
     }
+
+    router.post("/geodata").handler { ctx ->
+        val response: HttpServerResponse = ctx.response()
+        response.putHeader("content-type", "application/json")
+        val data = GeoData("GB", 3243242, "myTestUser")
+        response.end(gson.toJson(data))
+    }
+
+    httpServer.listen(8080)
 }
