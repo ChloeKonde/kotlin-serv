1f5a753803e4b4b2262e78fc6e03e3180a019bc9
Chloe Konde <konde.chloe@gmail.com>
2022-07-01 19:23:22 +0500

    remove vertx routes from main

--- src/main/kotlin/com/chloe/kotlinserv/MainVerticle.kt
+++ src/main/kotlin/com/chloe/kotlinserv/MainVerticle.kt
@@ -7,21 +7,4 @@ import io.vertx.ext.web.Router
 fun main(args: Array<String>) {
     val myServer = VertxHttpServer()
     myServer.start(8080)
-
-    val router = Router.router(myServer.vertx)
-    val gson = Gson()
-
-    router.get("/countrystats/").handler { ctx ->
-        val response: HttpServerResponse = ctx.response()
-        response.putHeader("content-type", "application/json")
-        val data = CountryStats("23-12-2000", "RUS", 3)
-        response.end(gson.toJson(data))
-    }
-
-    router.post("/geodata").handler { ctx ->
-        val response: HttpServerResponse = ctx.response()
-        response.putHeader("content-type", "application/json")
-        val data = GeoData("GB", 3243242, "myTestUser")
-        response.end(gson.toJson(data))
-    }
 }
--- src/main/kotlin/com/chloe/kotlinserv/VertxHttpServer.kt
+++ src/main/kotlin/com/chloe/kotlinserv/VertxHttpServer.kt
@@ -1,13 +1,31 @@
 package com.chloe.kotlinserv
 
+import com.google.gson.Gson
 import io.vertx.core.Vertx
+import io.vertx.core.http.HttpServerResponse
+import io.vertx.ext.web.Router
 
 class VertxHttpServer : HttpServer {
-    lateinit var vertx:Vertx
 
     override fun start(port: Int) {
-        vertx = Vertx.vertx()
+        val vertx = Vertx.vertx()
         val httpServer = vertx.createHttpServer()
+        val router = Router.router(vertx)
+        val gson = Gson()
+
+        router.get("/countrystats/").handler { ctx ->
+            val response: HttpServerResponse = ctx.response()
+            response.putHeader("content-type", "application/json")
+            val data = CountryStats("23-12-2000", "RUS", 3)
+            response.end(gson.toJson(data))
+        }
+
+        router.post("/geodata").handler { ctx ->
+            val response: HttpServerResponse = ctx.response()
+            response.putHeader("content-type", "application/json")
+            val data = GeoData("GB", 3243242, "myTestUser")
+            response.end(gson.toJson(data))
+        }
         httpServer.listen(8080)
     }
 }
