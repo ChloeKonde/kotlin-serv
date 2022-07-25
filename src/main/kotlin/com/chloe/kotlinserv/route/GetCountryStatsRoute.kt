6388d5f066dceaf0c4a33b793d74157e4e27ce06
Chloe Konde <konde.chloe@gmail.com>
2022-07-25 23:14:15 +0500

    refactor geoDataService

--- src/main/kotlin/com/chloe/kotlinserv/route/GetCountryStatsRoute.kt
+++ src/main/kotlin/com/chloe/kotlinserv/route/GetCountryStatsRoute.kt
@@ -4,29 +4,17 @@ import com.chloe.kotlinserv.http.HttpMethod
 import com.chloe.kotlinserv.http.HttpRequest
 import com.chloe.kotlinserv.http.HttpResponse
 import com.chloe.kotlinserv.http.HttpRoute
-import com.chloe.kotlinserv.utils.toJson
 import com.chloe.kotlinserv.service.GeoDataServiceImpl
+import com.chloe.kotlinserv.utils.toJson
 import com.google.inject.Inject
-import com.google.inject.name.Named
-import com.zaxxer.hikari.HikariDataSource
 
 class GetCountryStatsRoute @Inject constructor(
-    private val dataSource: HikariDataSource,
-    @Named("geoDataBatchDelay") private val geoDataBatchDelay: Long,
-    @Named("tableName") private val tableName: String,
-    @Named("databaseName") private val dbName: String
+    private val geoDataServiceImpl: GeoDataServiceImpl
 ) : HttpRoute {
     override val endpoint = "/countrystats"
     override val method = HttpMethod.GET
 
     override val processFunction = { request: HttpRequest ->
-        val geoDataServiceImpl = GeoDataServiceImpl(
-            ds = dataSource,
-            geoDataBatchDelay = geoDataBatchDelay,
-            tableName = tableName,
-            dbName = dbName
-        )
-
         val groupLocal = request.queryParameters.getValue("groupLocal").firstOrNull()
         val startDate = request.queryParameters.getValue("startDate").firstOrNull()
         val endDate = request.queryParameters.getValue("endDate").firstOrNull()
--- src/main/kotlin/com/chloe/kotlinserv/route/PostGeoDataRoute.kt
+++ src/main/kotlin/com/chloe/kotlinserv/route/PostGeoDataRoute.kt
@@ -4,28 +4,18 @@ import com.chloe.kotlinserv.http.HttpMethod
 import com.chloe.kotlinserv.http.HttpRequest
 import com.chloe.kotlinserv.http.HttpResponse
 import com.chloe.kotlinserv.http.HttpRoute
-import com.chloe.kotlinserv.utils.fromJson
 import com.chloe.kotlinserv.service.GeoDataServiceImpl
+import com.chloe.kotlinserv.utils.fromJson
 import com.google.gson.JsonSyntaxException
 import com.google.inject.Inject
-import com.google.inject.name.Named
-import com.zaxxer.hikari.HikariDataSource
 
 class PostGeoDataRoute @Inject constructor(
-    private val dataSource: HikariDataSource,
-    @Named("geoDataBatchDelay") private val geoDataBatchDelay: Long,
-    @Named("tableName") private val tableName: String,
-    @Named("databaseName") private val dbName: String
+    private val geoDataServiceImpl: GeoDataServiceImpl,
 ) : HttpRoute {
     override val endpoint = "/geodata"
     override val method = HttpMethod.POST
 
     override val processFunction = { request: HttpRequest ->
-        val batch = GeoDataServiceImpl(
-            ds = dataSource, geoDataBatchDelay = geoDataBatchDelay, tableName = tableName,
-            dbName = dbName
-        )
-
         if (request.body == null) {
             HttpResponse(
                 code = 400,
@@ -37,7 +27,7 @@ class PostGeoDataRoute @Inject constructor(
                 val data = request.body.fromJson()
                 val requestHeaders = request.requestHeaders["x-forwarded-for"]
 
-                batch.addToList(data, requestHeaders?.firstOrNull())
+                geoDataServiceImpl.addToList(data, requestHeaders?.firstOrNull())
 
                 HttpResponse(
                     code = 200,
